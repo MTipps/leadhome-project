867b17971d4eea179d50fe766dd385616dfd3e9c
Megan <mtipps101@gmail.com>
2021-05-20 17:31:16 +0200

    Added skeleton code

---
+++ components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="search">
+    <p class="search__text">See if we’re in your area</p>
+    <input type="text" class="search__textbox" />
+    <button class="search__button">Search</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style scoped></style>
